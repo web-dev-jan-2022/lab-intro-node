0ce32b9dd18bcaed16bad197a2dde6cd2dad0df8
breogann <fernandocostapradillo@gmail.com>
2022-04-14 12:44:30 +0200

    iteration 7

--- index.js
+++ index.js
@@ -5,22 +5,52 @@ class SortedList {
   }
 
   add(item) {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i] > item) {
-        this.items.splice(i - 1, 0, item);
-      }
+    //      1) should add a value while keeping the list sorted
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    if (pos < this.length + 1) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
     }
   }
 
-  get(pos) {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.items.length - 1];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b);
+    } else {
+      return 0;
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    // return this.items.sum() / this.lenght; this wouldn't work (of course not with the error case, but either for the number / length?):(
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b) / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
